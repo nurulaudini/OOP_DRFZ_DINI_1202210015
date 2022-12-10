f23425d8698a6cead86304ab9e60cca52830111b
nurulaudini <nurulaudiniii@gmail.com>
2022-12-10 07:58:35 +0700

    MODUL2

--- MODUL2/MainApp.java
+++ MODUL2/MainApp.java
@@ -1,6 +1,6 @@
 public class MainApp {
     public static void main (String[] arg) throws Exception{
-        perangkat p = new perangkat("sampan",,1.8f);
+        TransportasiAir p = new TransportasiAir ("sampan",,1.8f);
         p.informasi();
         System.out.println();
     }

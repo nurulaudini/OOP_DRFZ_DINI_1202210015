b88c7832718a1bc440aa732a59c6355058d1f02b
nurulaudini <nurulaudiniii@gmail.com>
2022-12-31 07:40:45 +0700

    MODUL3

---
+++ MODUL3/calculation.java
@@ -0,0 +1,65 @@
+package MODUL3;
+
+public class calculation implements Runnable{
+    public double radius;
+    public double side;
+    public double area;
+    public double phi = 3.14;
+
+    public void setSquare(double side){
+        this.side=side;
+            try{
+                if (side<1){
+                    throw new IllegalArgumentException();
+                } else{
+                    area=side*side;
+                }
+            }catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+    }
+    public double getSquare(){
+        return area;
+    }
+    public void setCircle(double radius){
+        this.radius=radius;
+        try {
+            if(radius<1){
+                throw new IllegalArgumentException();
+            }else{
+                area=phi*radius;
+            }
+        }catch(NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+    public double getCircle(){
+        return area;
+    }
+    public void setTrapezoid(double a,double b, double t){
+        try{
+            if (a<1 || b<1 || t<1){
+                throw new IllegalArgumentException();
+            }else{
+                area=((a+b)*t)/2;
+            }
+        }catch(NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+    public double getTrapezoid(){
+        return area;
+    }
+    @Override
+    public void run() {
+        System.out.println("---PROGRAM WILL START IN : ");
+        for (int i = 3; i>0 ; i--){
+            try{
+                System.out.println("Starting with thread " +i);
+                Thread.sleep(1500);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ MODUL3/mainApp.java
@@ -0,0 +1,62 @@
+package MODUL3;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class mainApp {
+    public static void main(String[]args)throws Exception{
+        boolean repeat = true;
+        Scanner input = new Scanner(System.in);
+        calculation calculation = new calculation();
+        int menu_program;
+
+        do{
+            System.out.println("---MENU PROGRAM PENGHITUNG LUAS---");
+            System.out.println("1. Square\n2. Circle\n3. Trapezoid\n0. Exit program");
+            try{
+                System.out.println("Select menu : ");
+                menu_program=input.nextInt();
+                switch(menu_program){
+                    case 1 :
+                        System.out.println("Enter the length of side of the square : ");
+                        double side = input.nextDouble();
+                        calculation.setSquare(side);
+                        calculation.run();
+                        System.out.println("the calculation result : " +calculation.getSquare());
+                        break;
+                    case 2 :
+                        System.out.println("Enter the radius of the circle : ");
+                        double radius = input.nextDouble();
+                        calculation.setCircle(radius);
+                        calculation.run();
+                        System.out.println("the calculation result : " +calculation.getCircle());
+                        break;
+                    case 3 :
+                        System.out.println("insert the side of the base of the trapezoid : ");
+                        double a = input.nextDouble();
+                        System.out.println("enter the upper side of the trapezoid : ");
+                        double b = input.nextDouble();
+                        System.out.println("enter the height of the trapezoid : ");
+                        double t = input.nextDouble();
+                        calculation.setTrapezoid(a, b, t);
+                        calculation.run();
+                        System.out.println("the calculation result : " +calculation.getTrapezoid());
+                        break;
+                    case 0 :
+                        System.out.println("Thankyou\n===PROGRAM HAS ENDED===");
+                    default :
+                        System.out.println("menu is not available, choose other menu please ^^");
+
+                    }
+                    repeat=false;
+                
+            }catch (InputMismatchException e){
+                System.out.println("THERE IS A ERROR !!! \n ERROS MUST BE AN INTEGER");
+                Scanner scanner;
+                input.next();
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        } while (repeat);
+        input.close();
+    }
+}
